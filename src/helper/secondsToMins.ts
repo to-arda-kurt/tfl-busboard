083185ed3ad31bb3c63ef75c51d087bc37948d82
unknown <paul7markets@hotmail.com>
2023-09-03 14:53:09 +0100

    clean

--- src/helper/secondsToMins.ts
+++ src/helper/secondsToMins.ts
@@ -1,18 +1,17 @@
-export const secondsToMins = (seconds : number) => {
-    const minutes = Math.floor(seconds / 60);
+export const secondsToMins = (seconds: number) => {
+	const minutes = Math.floor(seconds / 60);
 
-    if( minutes === 0){
-        return "< 1 min"
-    }
+	if (minutes === 0) {
+		return '< 1 min';
+	}
 
-    if(minutes === 1){
-        return "1 min"
-    }
+	if (minutes === 1) {
+		return '1 min';
+	}
 
-    if(minutes < 100){
-        return `${minutes} mins`
-    }
+	if (minutes < 100) {
+		return `${minutes} mins`;
+	}
 
-    return `> 99 mins`
-
-}
+	return `> 99 mins`;
+};
